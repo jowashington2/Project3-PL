7f7f6a84274ed57c80c5befab76fa80307d078b6
Joshua Washington <jdwashington195@gmail.com>
2024-12-10 18:34:30 -0500

    Implementation of Staff controllers

---
+++ staffController.js
@@ -0,0 +1,20 @@
+const Staff = require('../models/Staff');
+
+exports.createStaff = async (req, res) => {
+  try {
+    const newStaff = new Staff(req.body);
+    await newStaff.save();
+    res.status(201).json(newStaff);
+  } catch (error) {
+    res.status(500).json({ message: 'Error creating staff member', error });
+  }
+};
+
+exports.listStaff = async (req, res) => {
+  try {
+    const staff = await Staff.find();
+    res.status(200).json(staff);
+  } catch (error) {
+    res.status(500).json({ message: 'Error listing staff', error });
+  }
+};
